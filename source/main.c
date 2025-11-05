f2da77b4f3fd287e4d38407c3b22122ac3fc8985
Beukot <michalek7790@gmail.com>
2025-11-05 17:39:04 +0100

    added another paddle

--- source/main.c
+++ source/main.c
@@ -3,12 +3,17 @@
 #define  GBA_SW 160                                        //actual gba screen width
 #define  SW     120                                       //game screen width
 #define  SH     80                                     //game screen height
-#define RGB(r,g,b) ((r)+((g)<<5)+((b)<<10))                //15 bit, 0-31, 5bit=r, 5bit=g, 5bit=b 
+#define RGB(r,g,b) ((r)+((g)<<5)+((b)<<10))                //15 bit, 0-31, 5bit=r, 5bit=g, 5bit=b
+
+#define PADDLE_OFFSET 20
+#define PADDLE_WIDTH 4
+#define PADDLE_HEIGHT 30
 
 int lastFr = 0;
 int FPS = 0;
 
 u16 bgColor = RGB(8,12,16);
+u16 lineColor = RGB(4,6,8);
 u16 paddleColor = RGB(31, 31, 31);
 
 typedef struct {
@@ -19,6 +24,7 @@ typedef struct {
     u16 color;
 } Object;    
 Object player;
+Object enemy;
 
 void clearBackground()
 {
@@ -26,6 +32,12 @@ void clearBackground()
         for(int y=0;y<SH;y++){
             VRAM[y * GBA_SW + x] = bgColor;
         }
+    }
+
+
+    for (int y=0;y<SH;y++) {
+        VRAM[y * GBA_SW + SW / 2 - 1] = lineColor;
+        VRAM[y * GBA_SW + SW / 2] = lineColor;
     }
 }
 
@@ -45,28 +57,31 @@ void drawObject(Object* o) {
     }
 }
 
-void moveObject(Object* o, int dx, int dy) {
-    clearObject(o);
-    o -> x += dx;
-    o -> y += dy;
-    drawObject(o);
-}
+//this is useless when using pageflipping
+//void moveObject(Object* o, int dx, int dy) {
+//    clearObject(o);
+//    o -> x += dx;
+//    o -> y += dy;
+//    drawObject(o);
+//}
 
 void buttons()                                            
 {
     if(KEY_R ){ } 
     if(KEY_L ){ }
     
-    if(KEY_U ){ 
-        moveObject(&player, 0, -3); 
+    if(KEY_U ){
+        player.y -= 3;
+        drawObject(&player);
         if(player.y < 0){ 
             player.y = 0;
             drawObject(&player);
         }
     }
     
-    if(KEY_D ){ 
-        moveObject(&player, 0, 3); 
+    if(KEY_D ){
+        player.y += 3;
+        drawObject(&player);
         if(player.y > SH-1 - player.height){ 
             player.y = SH - player.height;
             drawObject(&player);
@@ -82,13 +97,18 @@ void buttons()
 }
 
 void init() {
-    
-    
-    player.x = 20;
-    player.y = SH / 2;
-    player.width = 4;
-    player.height = 30;
+
+    player.width = PADDLE_WIDTH;
+    player.height = PADDLE_HEIGHT;
+    player.x = PADDLE_OFFSET;
+    player.y = SH / 2 - PADDLE_HEIGHT / 2;
     player.color = paddleColor;
+
+    enemy.width = PADDLE_WIDTH;
+    enemy.height = PADDLE_HEIGHT;
+    enemy.x = SW - PADDLE_WIDTH - PADDLE_OFFSET;
+    enemy.y = SH / 2 - PADDLE_HEIGHT / 2;
+    enemy.color = paddleColor;
     
     drawObject(&player);
 }
@@ -111,6 +131,7 @@ int main() {
             clearBackground();
             buttons();
             drawObject(&player);
+            drawObject(&enemy);
             
 
             //frames per second---------------------------------------------------------- 
@@ -125,7 +146,7 @@ int main() {
             lastFr=REG_TM2D>>12;                                    //reset counter?
             
             //swap buffers---------------------------------------------------------------
-            while(*Scanline<160){}	                                         //wait all scanlines 
+            while(*Scanline<160){}	                                         //wait all scanlines
             if  ( DISPCNT&BACKB){ DISPCNT &= ~BACKB; VRAM=(u16*)VRAM_B;}      //back  buffer
             else{                 DISPCNT |=  BACKB; VRAM=(u16*)VRAM_F;}      //front buffer  
         }
