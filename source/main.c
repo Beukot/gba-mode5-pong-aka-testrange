17012329dec826afaaa230043dbe792363991da7
Beukot <michalek7790@gmail.com>
2025-11-05 22:58:52 +0100

    added movement for ball

--- source/main.c
+++ source/main.c
@@ -16,19 +16,23 @@ int FPS = 0;
 u16 bgColor = RGB(8,12,16);
 u16 lineColor = RGB(4,6,8);
 u16 paddleColor = RGB(31, 31, 31);
-u16 ballColor = RGB(26, 13, 26);
+u16 ballColor = RGB(0, 31, 0);
 
 typedef struct {
     int x;
     int y;
     int width;
     int height;
+    int speed;
     u16 color;
 } Object;
 
 Object player;
 Object enemy;
 Object ball;
+
+int ballDX = 1;
+int ballDY = 1;
 
 void clearBackground()
 {
@@ -56,9 +60,33 @@ void clearObject(Object* o) {
 void drawObject(Object* o) {
     for (int i = 0; i < o -> height; i++) {
         for (int j = 0; j < o -> width; j++) {
-            VRAM[(o -> x + j) + GBA_SW * (o -> y + i)] = paddleColor;
+            VRAM[(o -> x + j) + GBA_SW * (o -> y + i)] = o -> color;
         }
     }
+}
+
+void moveBall() {
+    ball.x += ball.speed * ballDX;
+    ball.y += ball.speed * ballDY;
+
+    if (ball.x > SW - BALL_SIZE) {
+        ball.x -= 1;
+        ballDX = -1;
+    }
+    if (ball.x < 0) {
+        ball.x += 1;
+        ballDX = 1;
+    }
+    if (ball.y > SH - BALL_SIZE) {
+        ball.y -= 1;
+        ballDY = -1;
+    }
+    if (ball.y < 0) {
+        ball.y += 1;
+        ballDY = 1;
+    }
+
+    drawObject(&ball);
 }
 
 //this is useless when using pageflipping
@@ -75,7 +103,7 @@ void buttons()
     if(KEY_L ){ }
     
     if(KEY_U ){
-        player.y -= 3;
+        player.y -= player.speed;
         drawObject(&player);
         if(player.y < 0){ 
             player.y = 0;
@@ -84,7 +112,7 @@ void buttons()
     }
     
     if(KEY_D ){
-        player.y += 3;
+        player.y += player.speed;
         drawObject(&player);
         if(player.y > SH-1 - player.height){ 
             player.y = SH - player.height;
@@ -106,18 +134,21 @@ void init() {
     player.height = PADDLE_HEIGHT;
     player.x = PADDLE_OFFSET;
     player.y = SH / 2 - PADDLE_HEIGHT / 2;
+    player.speed = 3;
     player.color = paddleColor;
 
     enemy.width = PADDLE_WIDTH;
     enemy.height = PADDLE_HEIGHT;
     enemy.x = SW - PADDLE_WIDTH - PADDLE_OFFSET;
     enemy.y = SH / 2 - PADDLE_HEIGHT / 2;
+    enemy.speed = 2;
     enemy.color = paddleColor;
 
     ball.width = BALL_SIZE;
     ball.height = BALL_SIZE;
     ball.x = SW / 2 - BALL_SIZE / 2;
     ball.y = SH / 2 - BALL_SIZE / 2;
+    ball.speed = 2;
     ball.color = ballColor;
     
     drawObject(&player);
@@ -158,10 +189,10 @@ int main() {
             buttons();
             drawObject(&player);
             drawObject(&enemy);
-            drawObject(&ball);
+            moveBall();
 
             if (paddleDirection == 1) {
-                enemy.y += 2;
+                enemy.y += enemy.speed;
                 drawObject(&enemy);
                 if(enemy.y > SH-1 - enemy.height){
                     enemy.y = SH - enemy.height;
@@ -171,7 +202,7 @@ int main() {
             }
 
             if (paddleDirection == 0) {
-                enemy.y -= 2;
+                enemy.y -= enemy.speed;
                 drawObject(&enemy);
                 if(enemy.y < 0){
                     enemy.y = 0;
